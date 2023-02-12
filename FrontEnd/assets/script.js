2adfaf7f612a99e9be5511bad98eca2e9a491cab
Caglar Aslan <caslantii@gmail.com>
2023-02-12 21:29:02 +0100

    WorkList

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,66 +1,59 @@
 let works;
 
 async function getWorks() {
-    let works;
-
     const res = await fetch('http://localhost:5678/api/works')
 
     works = await res.json();
 
-    console.log(works);
+    return works;
+}
 
-    function createGalery(work) {
-        const gallery = document.querySelector(".gallery");
-        for (let i = 0; i < work.length; i++) {
-            const fig = document.createElement("figure");
-            const figCaptions = document.createElement("figcaption");
-            figCaptions.innerText = work[i].title;
-            const workImg = document.createElement("img");
-            workImg.src = work[i].imageUrl;
-            workImg.crossOrigin = "anonymous";
-            gallery.appendChild(fig);
-            fig.appendChild(workImg);
-            fig.appendChild(figCaptions);
+function cleanGallery() {
+    document.querySelector(".gallery").innerHTML = "";
+}
 
-            //console.log(works[i].category.name = "Objets");
-            //console.log(works[i]);
-        }
+function createGalery(works) {
+    cleanGallery();
 
-        const boutonFiltrerObj = document.querySelector("#objets");
-        boutonFiltrerObj.addEventListener("click", function() { filterWorks("Objets") });
-        const boutonFiltrerApt = document.querySelector("#appartements");
-        boutonFiltrerApt.addEventListener("click", function() { filterWorks("Appartements") });
-        const boutonFiltrerHotel = document.querySelector("#hotelResto");
-        boutonFiltrerHotel.addEventListener("click", function() { filterWorks("Hotels & restaurants") });
-        const boutonTous = document.querySelector("#tous");
-        boutonTous.addEventListener("click", function() {
-            filterTous();
-        });
-    }
-    createGalery(works);
-
-    function filterWorks(filtredCategory) {
-        const Result = works.filter(category => category.category.name == filtredCategory);
-        const gallery = document.querySelector(".gallery").innerHTML = "";
-        createGalery(Result);
-    }
-
-    function filterTous() {
-        const gallery = document.querySelector(".gallery").innerHTML = "";
-        createGalery(works);
+    const gallery = document.querySelector(".gallery");
+    for (let i = 0; i < works.length; i++) {
+        const fig = document.createElement("figure");
+        const figCaptions = document.createElement("figcaption");
+        figCaptions.innerText = works[i].title;
+        const workImg = document.createElement("img");
+        workImg.src = works[i].imageUrl;
+        workImg.crossOrigin = "anonymous";
+        gallery.appendChild(fig);
+        fig.appendChild(workImg);
+        fig.appendChild(figCaptions);
     }
 }
 
+function filterWorks(filtredCategory) {
+    const result = works.filter(category => category.category.name == filtredCategory);
+    createGalery(result);
+}
+
+function filterTous() {
+    createGalery(works);
+}
+
+const boutonFiltrerObj = document.querySelector("#objets");
+boutonFiltrerObj.addEventListener("click", function() { filterWorks("Objets") });
+const boutonFiltrerApt = document.querySelector("#appartements");
+boutonFiltrerApt.addEventListener("click", function() { filterWorks("Appartements") });
+const boutonFiltrerHotel = document.querySelector("#hotelResto");
+boutonFiltrerHotel.addEventListener("click", function() { filterWorks("Hotels & restaurants") });
+const boutonTous = document.querySelector("#tous");
+boutonTous.addEventListener("click", function() {
+    filterTous();
+});
 
 
-
-
-
-
-
-
-getWorks();
-
+// Lancement du site
+getWorks().then(works => {
+    createGalery(works);
+});
 
 function loginMe() {
     //e.preventDefault();
@@ -74,7 +67,9 @@ function loginMe() {
 
 }
 
-const mail = document.getElementById("email");
+
+
+/*const mail = document.getElementById("email");
 const password = document.getElementById("password");
 const submitBnt = document.querySelector(".submitBtn");
 submitBnt.addEventListener("click", logintest);
@@ -84,7 +79,7 @@ function logintest() {
     console.log(mail.value, password.value);
 }
 
-logintest();
+logintest(); */
 
 /*
 
